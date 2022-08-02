1fced38ed8575d9ef92ce361c5dea55f13b79218
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-02 17:23:56 +0530

    Validate the confirm password field on every character entered by the user.

--- jsBasics.js
+++ jsBasics.js
@@ -1,11 +1,6 @@
-$(document).ready(function () {
-    $("body").on("click","#add",(function () {
-     $("body").append("<div class='parent'><input type='text' placeholder='Enter Class'/><input type='text' placeholder='Enter Board'/><input type='text' placeholder='Enter Marks'/><input type='text' placeholder='Enter Division'/><button id='remove'>-</button></div>")
-    }));
-    $("body").on("click","#remove",(function () {
-        $(this).parent().remove();
-        // $("body").append("<div class='parent'><input type='text' placeholder='Enter Class'/><input type='text' placeholder='Enter Board'/><input type='text' placeholder='Enter Marks'/><input type='text' placeholder='Enter Division'/><button id='add'>+</button><button id='remove'>-</button></div>")
-       }));
-    
-  });
-//   comment
+function check(){
+  $("#result").html("");
+if($("#password").val()!=$("#passwordMatch").val()){
+  $("#result").html("Password does not match");
+}
+}
