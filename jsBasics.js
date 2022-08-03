fad182ff88be0b8e327faad53274dfc9719c8092
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-03 15:00:46 +0530

    Create a paragraph and pop-up a alert message Clicked me when you clicked inside paragraph.

--- jsBasics.js
+++ jsBasics.js
@@ -1,16 +1,6 @@
 $(document).ready(function(){
-  $("#btnSubmit").click(function () {
-    if($("#first").val()==""){
-      $("#first").focus();
-      $("#first").css("background-color","red")
-      alert("Please enter first name")
-    }
-     
-     if($("#last").val()==""){
-      $("#last").focus();
-      $("#last").css("background-color","red")
-      alert("Please enter last name")
-    }
+  $("#para").click(function () {
+  alert("Clicked Me")
   });
      
 });
