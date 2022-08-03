60e70f84149846c2d29c50f74430135885210a6e
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-03 14:12:28 +0530

    Change the text color of first and last paragraph of each each element with test class, automatically after every 5 se

--- jsBasics.js
+++ jsBasics.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){ 
-  $("#parent").on("click","#btnCategory",(function () {
-     $("#categories").append("<li>New Category<button id='btnCategory'>+</button></li>");
-    }));
-    $("#parent").on("click","#btnProducts",(function () {
-      $(this).parent().parent().append("<li>New Product<button id='btnProducts'>+</button></li>")
-       }));
-
+$(document).ready(function(){
+  colours=["red","blue","pink","green","orange"] 
+i=0;
+setInterval(swapColor,1000);
+function swapColor(){
+  
+  console.log("changed")
+$("div.test").children().eq(0).animate().css("color",colours[i])
+$("div.test").children().eq(2).animate().css("color",colours[i])
+$("div.test").children().eq(3).animate().css("color",colours[i])
+$("div.test").children().eq(5).animate().css("color",colours[i])
+i++;
+if(i>4){
+  i=0
+}
+}
 
 });
