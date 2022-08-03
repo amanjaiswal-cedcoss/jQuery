1941a5f60b7eb7b8617cb0c06182c28f1305d138
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-03 14:55:01 +0530

    When a user clicks submit button leaving any of the input blank, pop-up a alert informing user about blank field and set focus on that field along with changing its background color to r

--- jsBasics.js
+++ jsBasics.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
-  colours=["red","blue","pink","green","orange"] 
-i=0;
-setInterval(swapColor,1000);
-function swapColor(){
-  
-  console.log("changed")
-$("div.test").children().eq(0).animate().css("color",colours[i])
-$("div.test").children().eq(2).animate().css("color",colours[i])
-$("div.test").children().eq(3).animate().css("color",colours[i])
-$("div.test").children().eq(5).animate().css("color",colours[i])
-i++;
-if(i>4){
-  i=0
-}
-}
-
+  $("#btnSubmit").click(function () {
+    if($("#first").val()==""){
+      $("#first").focus();
+      $("#first").css("background-color","red")
+      alert("Please enter first name")
+    }
+     
+     if($("#last").val()==""){
+      $("#last").focus();
+      $("#last").css("background-color","red")
+      alert("Please enter last name")
+    }
+  });
+     
 });
