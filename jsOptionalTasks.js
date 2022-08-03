f0bf115d6e26207e7cbd94e8ba11b72eb75eecb9
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-03 17:41:02 +0530

    Optional Task 8

--- jsOptionalTasks.js
+++ jsOptionalTasks.js
@@ -4,7 +4,4 @@ $(document).ready(function(){
       index=$("ul").children().eq(i).index()
       $("ul").children().eq(i).text(index+" "+$("ul").children().eq(i).text())
    }
-// $("body").on("click","#btn",(function(){
-//    alert($("ul").children().eq(0).text())
-// }))
 });
