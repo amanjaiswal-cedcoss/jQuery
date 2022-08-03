d69a1b0550099c1473096ee639eb0bb053fde70f
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-03 18:10:42 +0530

    Optional Task 10

--- jsOptionalTasks.js
+++ jsOptionalTasks.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-$("body").on("click","#btn",(function(){
-   $("#link").attr("href","https://www.youtube.com")
-   $("#link").html("You Tube")
-}))
+   $("#para_id").addClass("myClass")
+
 });
