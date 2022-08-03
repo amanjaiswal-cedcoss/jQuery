201f7db78eae2d897ecde63761fb20017ca240d9
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-03 11:56:11 +0530

    Create an array consisting names of some flowers. Add a textbox to suggest the flower names on every character typed.

--- jsBasics.js
+++ jsBasics.js
@@ -1,10 +1,15 @@
-
+flowers=["Lily","Rose","Lotus","Daffodils","Mariegold","Sunflower"];
 $(document).ready(function(){
-$("#parent").on("click","#even",(function(){
-$("#detailsTable").children().children("tr:even").addClass("lightgrey");
-}))
-$("#parent").on("click","#odd",(function(){
-  $("#detailsTable").children().children("tr:odd").addClass("blue");
-  }))
+  $("#flower").keyup(function(){
+    $matched="";
+  for(i=0;i<flowers.length;i++){
+    $item=$("#flower").val();
+    $match=flowers[i].substring(0,$item.length)
+    if($item==$match){
+      $matched+=" "+flowers[i];
+      }
+}
+$("#suggestions").html("Suggestions : "+$matched);
+})
 
 });
