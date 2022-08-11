6239bf39da62cb61043a1f629e8af0f2c22743e5
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-11 17:44:32 +0530

    Tag Changer (Optional Task)

--- jsOptionalTasks.js
+++ jsOptionalTasks.js
@@ -1,8 +1,35 @@
 $(document).ready(function () {
-  $("#btnToggle").click(function(){
-  $("div").toggle(
-    function () {
-      $("div").animate({ height: "500px" }, 500);
-    }
-  )})
+  $arrAttributes=[]
+  $("#btnChangeClass").click(function(){
+    $getAttributes(); 
+    $changeClass();
+    $("#btnChangeClass").attr("disabled","disabled")
+  })
+  function $changeClass(){
+    $arr=$("body").find("h1")
+       for(i=0;i<$arr.length;i++){
+       
+      if($($arr[i]).attr("class")!="bingo"){
+        $txt=$($arr[i]).html()
+        $($arr[i]).replaceWith("<h2>"+$txt+"</h2>");
+      }
+      }
+  }
+  function $getAttributes() {
+    $("h1").each(function(j) {
+        arrObj=[];
+        $.each(this.attributes,function(i,a){
+            obj={id:i+1,name:a.name,value:a.value}
+            arrObj.push(obj);
+          })
+          $("#divDisp").append("<h3>"+parseInt(j+1)+". "+arrObj.length+" attributes</h3>")
+          for(i=0;i<arrObj.length;i++){
+            console.log("name=",arrObj[i].name)
+          $("#divDisp").append("<p>"+arrObj[i].id+"."+arrObj[i].name+":"+arrObj[i].value+"</p>")
+          }
+    })
+console.log($arrAttributes)
+
+}
+
 });
